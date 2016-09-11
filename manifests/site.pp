ee5f52337d40b752a5087bb2d3213105b216a9f1
James E. Jones <velocity303@gmail.com>
2016-09-11 18:04:26 -0500

    clean up site.pp to default

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,24 +4,6 @@ Package {
   allow_virtual => true,
 }
 
-Firewall {
-  before  => Class['profile::firewall::post'],
-  require => Class['profile::firewall::pre'],
-}
-
-class { ['profile::firewall::pre', 'profile::firewall::post']: }
-
-resources { 'firewall':
-  purge => true,
-}
-
-include firewall
-
 node default {
-  if !empty( $trusted['extensions']['pp_role'] ) {
-    include "role::${trusted['extensions']['pp_role']}"
-  }
 }
-if hiera('is_pe') == 'false'{
-  hiera_include('classes')
-}
+
