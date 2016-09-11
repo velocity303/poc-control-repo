a7cfcbf735858cdcccf2744d6619b909e8260e30
James E. Jones <velocity303@gmail.com>
2016-09-11 18:03:10 -0500

    add ftp example

---
+++ site/profile/manifests/ftp.pp
@@ -0,0 +1,26 @@
+class profile::ftp {
+   # FTP Server filename
+  $dirs = [
+    '/opt/sni/',
+    '/opt/sni/ftpcfg'
+  ]
+
+  case $::hostname {
+    /(prod|prd|admin)/: { $ftp_server_name = 'ftp.example.com' }
+    default:            { $ftp_server_name = 'ftpdev.example.com' }
+  }
+
+  file { $dirs:
+    ensure => directory,
+    owner  => root,
+    group  => root,
+  }
+
+  file { "${dirs[1]}/ftp_server_name":
+    ensure  => file,
+    content => $ftp_server_name,
+    owner   => root,
+    group   => root,
+  }
+
+}
