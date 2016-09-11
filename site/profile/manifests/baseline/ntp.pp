7b709d6d20d90a12dde041625d7fddd3e6a82f32
James E. Jones <velocity303@gmail.com>
2016-09-11 18:06:52 -0500

    update to ensure it is referring to top scope for virtual fact

--- site/profile/manifests/baseline/ntp.pp
+++ site/profile/manifests/baseline/ntp.pp
@@ -1,5 +1,5 @@
 class profile::baseline::ntp {
-  case $virtual {
+  case $::virtual {
     'physical': { include profile::baseline::ntp::blade }
     'vmware':   { include profile::baseline::ntp::vm }
   }
