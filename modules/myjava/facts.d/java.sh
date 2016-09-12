c4a6bf3463a250af127681d74d61790102fa7752
James E. Jones <james.jones@puppet.com>
2016-09-11 22:24:52 -0500

    Update path for java version

--- modules/myjava/facts.d/java.sh
+++ modules/myjava/facts.d/java.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-JAVA_VERSION=$(java -version 2>&1 | grep version | cut -d'"' -f2 | cut -d '"' -f1)
+JAVA_VERSION=$(/opt/oracle/java/bin/java -version 2>&1 | grep version | cut -d'"' -f2 | cut -d '"' -f1)
 echo "java_ver=$JAVA_VERSION"
