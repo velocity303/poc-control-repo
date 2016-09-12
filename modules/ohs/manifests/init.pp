c0d94745327fdad17ef0092c48dd4c2785908264
James E. Jones <velocity303@gmail.com>
2016-09-11 20:27:24 -0500

    update ohs template and exec resource

--- modules/ohs/manifests/init.pp
+++ modules/ohs/manifests/init.pp
@@ -1,12 +1,17 @@
 #A class to install OHS from scratch
-class ohs {
+class ohs (
+  $oracle_home = '/opt/oracle/middleware/ohs',
+  $middleware_home = '/opt/oracle/middleware',
+  $instance_home = '/opt/oracle/middleware/ohs/instances/instance1',
+  $static_port_ini_file = '/opt/sw/prd_middleware/StaticPorts/staticports.ini',
+){
   file { '/opt/sw/prd_middleware/OHS/11g/11_1_1_9_0/OHS_ServerInstall.rsp':
     ensure  => present,
     content => template('ohs/ohs_serverinstall.rsp.erb'),
   }->
   exec { 'install ohs':
-    command     => '/opt/sw/prd_middleware/OHS/11g/11_1_1_9_0/Disk1/runInstaller -silent -responseFile /opt/sw/prd_middleware/OHS/11g/11_1_1_9_0/OHS_ServerInstall.rsp -force 2>&1 | tee -a /var/schneider/logs/Middleware/ohs_install.log',
-    path        => ['/bin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
-    refreshonly => true,
+    command => '/opt/sw/prd_middleware/OHS/11g/11_1_1_9_0/Disk1/runInstaller -silent -responseFile /opt/sw/prd_middleware/OHS/11g/11_1_1_9_0/OHS_ServerInstall.rsp -force 2>&1 | tee -a /var/schneider/logs/Middleware/ohs_install.log',
+    path    => ['/bin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
+    creates => $oracle_home,
   }
 }
